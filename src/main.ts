f50cd94486ff6cd1237a4c6571d3fe4c5523aef4
Jonster5 <onlyaccelerating@gmail.com>
2023-07-03 22:58:35 -0400

    init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ECS } from 'raxis';
+import './style.scss';
+import { CanvasSettings, KeysToTrack, defaultPlugins } from 'raxis-plugins';
+
+const app = document.getElementById('app');
+
+const ecs = new ECS()
+	.insertPlugins(...defaultPlugins)
+	.insertResource(new CanvasSettings({ target: app, width: 2000 }))
+	.insertResource(new KeysToTrack([]));
+
+ecs.run();
